dee2aff47ad58ed5ae63d93b45acda166d773fff
Stevovenom <stephenokoth2002@gmail.com>
2023-08-12 04:23:52 -0700

    printig combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 	int i, j, k;
 	int count = 0;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j < 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k < 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				if (count != 0)
 				{
