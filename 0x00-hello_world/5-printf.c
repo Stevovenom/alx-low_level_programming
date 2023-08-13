b831233deb5a791c70bb8289b770b4b62f5499e7
Stevovenom <stephenokoth2002@gmail.com>
2023-08-13 04:43:44 -0700

    prints without warning in 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the given text followed by a new line
+ *
+ * Return: is always 0 showing success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
