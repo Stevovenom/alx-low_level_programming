35b24b11f43161c98badd3aa44ee843466a63686
Stevovenom <stephenokoth2002@gmail.com>
2023-08-15 17:28:53 -0700

    checks for alphabetic character in uppercase or lowercase and gives it in integers

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to be refenced
+ * Return: return 1 for lowercase/upper 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	{
+
+		return (1);
+	}
+	return (0);
+}
