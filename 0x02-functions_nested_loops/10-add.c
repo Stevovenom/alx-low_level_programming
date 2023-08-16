7b92696cd50c8c1e775818b99ee18a8c64a5ad94
Stevovenom <stephenokoth2002@gmail.com>
2023-08-15 18:48:29 -0700

    Adds two numbers and outputs the results

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @n1: The first number as input
+ * @n2: The second number as input
+ * Return: Returns the integer value-results
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table from 0
+ *
+ * Return: always 0 showing succes
+ */
+void times_table(void)
+{
