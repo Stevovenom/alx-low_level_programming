d351452b5a8c1d72d40d1965b63e423ac299ee62
Stevovenom <stephenokoth2002@gmail.com>
2023-08-17 07:32:45 -0700

    multiplies two integers using a nd b as the reference characters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: the first input of the user
+ * @b: the second input of the user
+ * Return: shows 0 for success
+ */
+int mul(int a, int b)
+{
+
+	int result = a * b;
+
+	return (result);
+}
