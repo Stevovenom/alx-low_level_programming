39ff4fd18816726a1a00a67028c2373008c1e326
Stevovenom <stephenokoth2002@gmail.com>
2023-08-22 15:11:44 -0700

    The function takes a pointer to an int as a parameter and it u[dates it with a value of 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to int and updates its value
+ * @n: its a variable that points to an int
+ * Return: returns value of the pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The task is all about pointers, arrays and stringd and basically the introductory part of it. I'm going to use my own generated main.h header file and also create a putchar.c to use.
