5c25daa8028bca9e3ce5b49b25cc5c42c721d848
Stevovenom <stephenokoth2002@gmail.com>
2023-08-24 14:51:24 -0700

    using the man strncpy, have made the necessary changes to the command tha best suites my function so as to achieve the result

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - it copies a string from src to the dest
+ * @dest: the pointer to which the src will be copied to
+ * @src: the source pointer that will be copied to dest
+ * @n: The number of elements to be copied
+ * Return: should be the dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
