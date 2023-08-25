ea8548c1a1872683362406d52653d2f005751a2d
Stevovenom <stephenokoth2002@gmail.com>
2023-08-25 07:31:32 -0700

    The function compares two strings that is, source pointer s1 and s2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - it compares string s1 to s2
+ * @s1: denotes the first string to be compared
+ * @s2: denotes the second string to be compared
+ * Return: 0 if equal, positive if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
