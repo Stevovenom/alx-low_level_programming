3fe8750e4d5725045c22fb9793e9c72ab26892a9
Stevovenom <stephenokoth2002@gmail.com>
2023-08-30 05:55:03 -0700

    The strpbrk searches a string for any of a set of bytes and it returns a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - locates thfirst occurence of a string
+ * @s: the pointer to the string to be searched
+ * @accept: contains the bytes to be searched for
+ * Return:  pointer to the byte in s that matches one of the bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
