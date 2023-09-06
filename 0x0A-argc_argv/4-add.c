14daf547864eb6b74a5bc7bfce9d3b00f8ed12d3
stevovenom <stephenokoth2002@gmail.com>
2023-09-06 12:33:40 -0700

    The program adds only two positive numbers, returns a 0 if no arguments is passed, error for negative or noon-integer arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,15 +22,14 @@ int main(int argc, char *argv[])
 for (i = 1; i < argc; i++)
 {
 	num = atoi(argv[i]);
-
 if (num <= 0)
 {
 	printf("Error\n");
 	return (1);
 }
-
 sum += num;
 }
+
 printf("%d\n", sum);
 return (0);
 }
