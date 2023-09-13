0af13b33f817b47136dae5281419dd8730e4bf12
stevovenom <stephenokoth2002@gmail.com>
2023-09-13 03:16:46 -0700

    The use of typedef to refer to the alias of struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: the pointer to the age of the dog
  * @owner: the details of the owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
