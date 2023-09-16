b5297ac981f8496c5eed672e1a5a983629425a91
stevovenom <stephenokoth2002@gmail.com>
2023-09-16 02:07:40 -0700

    Task to print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,8 +7,7 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	int num;
+	unsigned int i, num;
 
 	va_list args;
 
@@ -16,14 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args, int);
+		num = va_arg(args, const unsigned int);
 		printf("%d", num);
 
-		if (i < n - 1)
-		{
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		}
 	}
-
+	printf("\n");
 	va_end(args);
 }
