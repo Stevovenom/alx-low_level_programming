5770e9bce1fa8b839b5f8a46574199facd045c19
Stevovenom <stephenokoth2002@gmail.com>
2023-10-07 03:42:41 -0700

    The fully corrected template

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,8 +50,7 @@ int main(int argc, char *argv[])
 		nchar = read(file_from, buf, 1024);
 		if (nchar == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, 1024);
-
+		nwr = write(file_to, buf, nchar);
 		if (nwr == -1)
 		error_file(0, -1, argv);
 	}
