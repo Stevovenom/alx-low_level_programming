5ec4de288d2d8e2a40a96e610e509bd8daf09543
stevovenom <stephenokoth2002@gmail.com>
2023-10-27 20:48:43 -0700

    updation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,13 +2,15 @@
 /**
  * clear_bit - function that sets the value of a bit to 0 at a given index
  * @n: the pointer to the character whose binary value is to be set
- * index: the position of the binary value to be manipulated
+ * @index: the position of the binary value to be manipulated
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int mask;
+
 	mask = 1;
+
 	mask = mask << index;
 
 	if (index > sizeof(unsigned long int) * 8 || n == NULL)
