d8e9d4cd2dfbcc53d65b384698a977f433479dac
Stevovenom <stephenokoth2002@gmail.com>
2024-04-30 06:10:11 -0700

    The function that searches for a value in a sorted array of integers using the Jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+/**
+ * jump_search - searches for a value using the Jump search algorithm
+ * @array: the pointer to the first element
+ * @size: the size of the array
+ * @value: the value to be searched
+ * Return: -1 if value is not present in the array
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, step, jump;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array [%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	jump = jump > size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,8 +5,13 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
